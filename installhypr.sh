a81b8690fce93600c8cf8c182024cf1d4d725f9e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-10 21:45:11 +0900

    Update installhypr.sh

--- installhypr.sh
+++ installhypr.sh
@@ -1,3 +1,6 @@
-sudo pacman -S --needed hyprland waybar alacritty clipman hyprpaper ttf-font-awesome wofi dunst xdg-desktop-portal-hyprland qt5-wayland qt6-wayland
+sudo pacman -S --needed waybar alacritty hyprpaper ttf-font-awesome wofi dunst xdg-desktop-portal-hyprland qt5-wayland qt6-wayland
+yay -S clipman
+
+sudo pacman -S --needed hyprland
 
 yay -S pfetch
